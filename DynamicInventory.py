33778e242b3d9f566b5c47ced362df7f88955b6b
chibuezer <94007803+chibuezer@users.noreply.github.com>
2022-03-09 06:31:29 -0700

    Update DynamicInventory.py

--- DynamicInventory.py
+++ DynamicInventory.py
@@ -1,7 +1,6 @@
 #!/bin/python
 # Shebang Line Should Point Python Installation Path
 """
-Author Landmark Technologies
 Fetech List of Servers from AWS and group based on EC2
 instance tag name and value.
 """
